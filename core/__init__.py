6e6c852d1894fcb2b8239987e8ec00d564996e72
limapedro <Pedrolimasilva17@gmail.com>
2020-12-27 19:32:18 -0300

    Add NLU as a way to process speech

--- core/__init__.py
+++ core/__init__.py
@@ -10,3 +10,18 @@ class SystemInfo:
         now = datetime.datetime.now()
         answer = 'The time now is {} {}'.format(now.hour, now.minute)
         return answer
+
+'''
+    @staticmethod
+    def get_time():
+        now = datetime.datetime.now()
+        answer = 'The date is {} {}'.format(now.hour, now.minute)
+        return answer
+
+'''
+
+    @staticmethod
+    def get_year():
+        now = datetime.datetime.now()
+        answer = 'The current year is {}'.format(now.hour, now.minute)
+        return answer
